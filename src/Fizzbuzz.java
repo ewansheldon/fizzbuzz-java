49eb3a675d7dc023c2203394d7337387a7fa3bd2
Ewan Sheldon <ewansheldon@gmail.com>
2016-12-07 15:03:56 +0000

    fizzbuzz complete

--- src/Fizzbuzz.java
+++ src/Fizzbuzz.java
@@ -1,4 +1,17 @@
 public class Fizzbuzz {
+
+    public static void main(String[] args) {
+
+        Fizzbuzz fizzbuzz = new Fizzbuzz();
+
+        for (int j=1; j < 101; j++) {
+            System.out.println(fizzbuzz.play(j));
+//              System.out.println("HELLO WORLD");
+
+        }
+
+    }
+
     public String play(int i) {
         if (i % 15 == 0) {
             return "fizzbuzz";
@@ -11,3 +24,4 @@ public class Fizzbuzz {
         }
     }
 }
+
--- test/FizzbuzzTest.java
+++ test/FizzbuzzTest.java
@@ -8,21 +8,29 @@ class FizzbuzzTest {
 
     @Test
     void returnFizzWhenDivisibleBy3() {
+
         assertEquals("fizz", fizzbuzz.play(3));
+        assertEquals("fizz", fizzbuzz.play(9));
     }
 
     @Test
     void returnBuzzWhenDivisibleBy5() {
+
         assertEquals("buzz", fizzbuzz.play(5));
+        assertEquals("buzz", fizzbuzz.play(40));
     }
 
     @Test
     void returnFizzbuzzWhenDivisibleBy3And5() {
+
         assertEquals("fizzbuzz", fizzbuzz.play(15));
+        assertEquals("fizzbuzz", fizzbuzz.play(60));
     }
 
     @Test
     void returnIntegerIfNotDivisibleBy3Or5() {
+
         assertEquals("4", fizzbuzz.play(4));
+        assertEquals("82", fizzbuzz.play(82));
     }
 }
